a5cef6ec15aa43cd48b0d58596d00c1781fa79c7
ycseng912 <dora.930912@gmail.com>
2025-11-17 19:53:36 +0800

    rename the file

---
+++ Assignment/AssignmentIV/C/hash_fn.h
@@ -0,0 +1,24 @@
+/*
+   ========================================
+   hash_fn.h — declare your hash functions
+   ========================================
+
+   Description:
+    This file contains the hash functions for integer and string keys.
+
+   Development History:
+    - 2025/11/17: Initial implementation from prof. Huang
+                  Update developer information
+
+   Developer: Yung-Chi Tseng <a1121411@mail.yzu.edu.tw>
+ */
+#ifndef HASH_FN_H
+#define HASH_FN_H
+
+#include <stdio.h>
+#include <string.h>
+
+int myHashString(const char* str, int m);
+int myHashInt(int key, int m);
+
+#endif
